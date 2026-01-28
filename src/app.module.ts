059ef8441eea144bccc32abc6b7aebe831a0beaf
younousfofana <younousfofana18@gmail.com>
2026-01-28 16:56:36 +0100

    INSTALL @nestjs/config (DEPENDANCE FOR HANDLE .ENV FILE), pg (POSTGRES DEPENDANCE FOR CONNECT TO DATABASE), UPDATE APP MODULE (ADD CONFIG), CREATE CONFIG  FOLDER (DBCONFIG (DEV ENVIRONMENT), DBCONFIGPRODUCTION (PRODUCTION ENVIRONMENT))

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import dbconfig from './config/dbconfig';
+import dbconfigproduction from './config/dbconfigproduction';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    
+    ConfigModule.forRoot({
+      isGlobal:  true, // Le module ConfigModule est disponible partout dans l’application, automatiquement.
+      expandVariables: true, // Permet d’utiliser des variables dans d’autres variables dans ton .env
+      load: [dbconfig, dbconfigproduction]
+    }),
+
+    TypeOrmModule.forRootAsync({
+      useFactory: process.env.NODE_ENV == "production" ? dbconfigproduction : dbconfig 
+    })
+  ],
+  
   controllers: [AppController],
   providers: [AppService],
 })
