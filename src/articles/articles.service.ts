050d3f790b6a0fb0dad944b91dbef59f6bf32a1d
younousfofana <younousfofana18@gmail.com>
2026-01-30 12:03:16 +0100

    UPDATE ARTICLES SERVICE (ADD COMMENTD)

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Articles } from 'src/entities/articles.entity';
+import { Families } from 'src/entities/families.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -9,6 +10,11 @@ export class ArticlesService {
     constructor(@InjectRepository(Articles) private readonly articlesRepo : Repository<Articles>){}
 
     public async findAll() : Promise<Articles[]>{
-        return await this.articlesRepo.find();
+        // relations: ['famille'] dans la methode find Permet d'eviter les erreurs lorsque pour un article donne on voudrais 
+        // recuperer dans la requete la famille a laquelle il est rattache l autre solution serait d utiliser le lazy loading 
+        // dans le resolver 
+        return await this.articlesRepo.find({
+            relations: ['famille']
+        });
     }
 }
